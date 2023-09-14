33b9c5066b0a83d80f2578ac4448c1256d72a79e
Frederic Branca <frederic.branca@gmail.com>
2023-09-14 22:06:36 +0200

    test Provider

--- src/Document/Champion.php
+++ src/Document/Champion.php
@@ -16,27 +16,60 @@ class Champion
      */
     protected $id;
 
+    public function getId(): ?string
+    {
+        return $this->id;
+    }
+
     /**
      * @MongoDB\Field(type="string")
      */
     protected $type;
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
 
     /**
      * @MongoDB\Field(type="string")
      */
     protected $format;
 
+    public function getFormat(): ?string
+    {
+        return $this->format;
+    }
+
     /**
      * @MongoDB\Field(type="string")
      */
     protected $version;
+
+    public function getVersion(): ?string
+    {
+        return $this->version;
+    }
 
     /**
      * @MongoDB\EmbedOne(targetDocument=ChampionData::class)
      */
     protected $data;
 
-    // getters and setters
+    public function getData(): ?ChampionData
+    {
+        return $this->data;
+    }
+
+    // /**
+    //  * @MongoDB\EmbedOne(targetDocument=Keys::class)
+    //  */
+    // protected $keys;
+
+    // public function getKeys(): ?Keys
+    // {
+    //     return $this->keys;
+    // }
 }
 
 /** 
@@ -49,20 +82,40 @@ class ChampionData
      */
     protected $id;
 
+    public function getId(): ?string
+    {
+        return $this->id;
+    }
+
     /**
      * @MongoDB\Field(type="string")
      */
     protected $key;
+
+    public function getKey(): ?string
+    {
+        return $this->key;
+    }
 
     /**
      * @MongoDB\Field(type="string")
      */
     protected $name;
 
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
     /**
      * @MongoDB\Field(type="string")
      */
     protected $title;
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
 
     // getters and setters
 }
