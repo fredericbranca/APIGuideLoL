016fb2932717753f41d9f28064fd764f73e50ad6
Frederic Branca <frederic.branca@gmail.com>
2023-09-21 01:04:56 +0200

    Optimise l'import de champion dans la db

--- src/Controller/DataImportController.php
+++ src/Controller/DataImportController.php
@@ -20,40 +20,27 @@ class DataImportController extends AbstractController
 
             foreach ($data as $id => $championData) {
                 // Recherche du champion par son ID
-                $championExiste = $dm->getRepository(Champion::class)->findOneBy(['id' => $id]);
+                $champion = $dm->getRepository(Champion::class)->findOneBy(['id' => $id]);
 
-                // Si le champion n'existe pas encore, on le crée
-                if ($championExiste == null) {
-                    $champion = new Champion();
+                // Création du champion s'il n'existe pas encore
+                if (!$champion) {
+                    $champion = new Champion;
                     $champion->setId($id);
-                    $champion->setIdChamp($championData['id']);
-                    $champion->setKey($championData['key']);
-                    $champion->setName($championData['name']);
-                    $champion->setTitle($championData['title']);
-                    $champion->setImage($championData['image']);
-                    $champion->setSkins($championData['skins']);
-                    $champion->setLore($championData['lore']);
-                    $champion->setBlurb($championData['blurb']);
-                    $champion->setTags($championData['tags']);
-                    $champion->setPartype($championData['partype']);
-                    $champion->setSpells($championData['spells']);
-                    $champion->setPassive($championData['passive']);
                     $dm->persist($champion);
-                } else {
-                    $championExiste->setIdChamp($championData['id']);
-                    $championExiste->setKey($championData['key']);
-                    $championExiste->setName($championData['name']);
-                    $championExiste->setTitle($championData['title']);
-                    $championExiste->setImage($championData['image']);
-                    $championExiste->setSkins($championData['skins']);
-                    $championExiste->setLore($championData['lore']);
-                    $championExiste->setBlurb($championData['blurb']);
-                    $championExiste->setTags($championData['tags']);
-                    $championExiste->setPartype($championData['partype']);
-                    $championExiste->setSpells($championData['spells']);
-                    $championExiste->setPassive($championData['passive']);
-                    $dm->persist($championExiste);
                 }
+
+                $champion->setIdChamp($championData['id']);
+                $champion->setKey($championData['key']);
+                $champion->setName($championData['name']);
+                $champion->setTitle($championData['title']);
+                $champion->setImage($championData['image']);
+                $champion->setSkins($championData['skins']);
+                $champion->setLore($championData['lore']);
+                $champion->setBlurb($championData['blurb']);
+                $champion->setTags($championData['tags']);
+                $champion->setPartype($championData['partype']);
+                $champion->setSpells($championData['spells']);
+                $champion->setPassive($championData['passive']);
             }
 
             $dm->flush();
